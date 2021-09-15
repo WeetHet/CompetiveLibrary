b05a973125e3df5a1bd4b188e4fce693f1290cea
Moonlight_Ghost <43210583+MoonlightGhost@users.noreply.github.com>
2021-09-15 12:59:36 +0300

    Add files via upload

---
+++ Snippets/Geometry/chull.h
@@ -0,0 +1,32 @@
+vector<int> chull(vector<pt> &v) {
+        int ind = (int) (min_element(all(v), cmp) - begin(v));
+        //        int ind = 0;
+        vector<int> cand, chull{ind};
+        
+        for (int i = 0; i < sz(v); ++i)
+            if (v[i] != v[ind])
+                cand.push_back(i);
+        
+        sort(all(cand), [&](int a, int b) {
+            pt x = v[a] - v[ind], y = v[b] - v[ind];
+            T t = cross(x, y);
+            return t != 0 ? t > 0 : norm(x) < norm(y);
+        });
+        
+        for (auto c: cand) {
+            while (sz(chull) > 1 && make(v[chull[sz(chull) - 2]], v[chull.back()], v[c]) <= 0)
+                chull.pop_back();
+            
+            chull.push_back(c);
+        }
+        return chull;
+    }
+    
+    vector<pt> hull(vector<pt> v) {
+        vector<int> w = chull(v);
+        vector<pt> res;
+        for (auto &t: w)
+            res.push_back(v[t]);
+        return res;
+    }
+    
---
+++ Snippets/String/sa_lin.h
@@ -0,0 +1,60 @@
+vector<int> sa_is(const vector<int>& s, int upper) {
+    int n = sz(s); if (!n) return {};
+    vector<int> sa(n);
+    vector<bool> ls(n);
+    for (int i = n - 2; i >= 0; --i) ls[i] = s[i] == s[i + 1] ? ls[i + 1] : s[i] < s[i + 1];
+    
+    vector<int> sum_l(upper), sum_s(upper);
+    for(int i = 0; i < n; ++i)
+        (ls[i] ? sum_l[s[i]+1] : sum_s[s[i]])++;
+    
+    for (int i = 0; i < upper; ++i) {
+        if (i) sum_l[i] += sum_s[i-1];
+        sum_s[i] += sum_l[i];
+    }
+    
+    auto induce = [&](const vector<int>& lms) {
+        fill(all(sa), -1);
+        vector<int> buf = sum_s;
+        for (int d: lms) if (d != n) sa[buf[s[d]]++] = d;
+        
+        buf = sum_l;
+        sa[buf[s[n - 1]]++] = n - 1;
+        for (int i = 0; i < n; ++i) {
+            int v = sa[i] - 1;
+            if (v >= 0 && !ls[v]) sa[buf[s[v]]++] = v;
+        }
+        buf = sum_l;
+        for (int i = n - 1; i >= 0; --i)  {
+            int v = sa[i] - 1;
+            if (v >= 0 && ls[v]) sa[--buf[s[v] + 1]] = v;
+        }
+    };
+    
+    vector<int> lms_map(n + 1, -1), lms; int m = 0;
+    for (int i = 1; i < n; ++i)
+        if (!ls[i - 1] && ls[i]) lms_map[i] = m++, lms.push_back(i);
+    
+    induce(lms);
+    vector<int> sorted_lms;
+    for (auto v: sa) if (lms_map[v] != -1) sorted_lms.push_back(v);
+    
+    vector<int> rec_s(m); int rec_upper = 0;
+    for (int i = 1; i < m; ++i) {
+        int l = sorted_lms[i - 1], r = sorted_lms[i];
+        int end_l = lms_map[l] + 1 < m ? lms[lms_map[l] + 1] : n;
+        int end_r = lms_map[r] + 1 < m ? lms[lms_map[r] + 1] : n;
+        
+        bool same = 0;
+        if (end_l - l == end_r - r) {
+            for (; l < end_l && s[l] == s[r]; ++l,++r);
+            if (l != n && s[l] == s[r]) same = 1;
+        }
+        rec_s[lms_map[sorted_lms[i]]] = (rec_upper += !same);
+    }
+    
+    vector<int> rec_sa = sa_is(rec_s, rec_upper + 1);
+    for (int i = 0; i < m; ++i) sorted_lms[i] = lms[rec_sa[i]];
+    induce(sorted_lms);
+    return sa;
+}
